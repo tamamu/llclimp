a58dba90b0193b873ef4264be719a5cf4a4b3aa4
tamamu <tamamu.1r1s@gmail.com>
2016-11-23 21:06:07 +0900

    add mainloop

--- src/main.ml
+++ src/main.ml
@@ -1,8 +1,4 @@
-open Llvm
+let main () =
+  Toplevel.main_loop ()
 
-let _ =
-  let llctx = Llvm.global_context () in
-  let llmem = Llvm.MemoryBuffer.of_file Sys.argv.(1) in
-  let llm = Llvm_bitreader.parse_bitcode llctx llmem in
-  Llvm.dump_module llm;
-  ()
+let () = main ()
---
+++ src/toplevel.ml
@@ -0,0 +1,19 @@
+
+let rec main_loop () : unit =
+  Printf.printf "ready> %!";
+  match
+    let lexbuf = Syntax.create_lexbuf @@
+      Sedlexing.Utf8.from_channel stdin in
+    Syntax.parse_statement lexbuf
+  with
+  | Some statement -> begin
+      print_endline @@ Ast.show_statement statement;
+      main_loop ()
+    end
+
+  | exception Syntax.ParseError e -> begin
+      print_endline @@ Syntax.string_of_ParseError e;
+      main_loop ()
+    end
+
+  | None -> print_newline ()
