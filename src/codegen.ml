81030d791e2cd14a334c4d5d2d3563132c1a66c3
tamamu <tamamu.1r1s@gmail.com>
2016-12-10 17:11:25 +0900

    update: WIP able to run

--- src/codegen.ml
+++ src/codegen.ml
@@ -9,41 +9,57 @@ let builder = builder context
 let symbol_tbl:(string, llvalue) Hashtbl.t = Hashtbl.create 10
 let bit_type = i1_type context
 let integer_type = i32_type context
-let address_type = pointer_type integer_type
+let address_type = pointer_type (void_type context)
 let float_type = float_type context
 let character_type = i8_type context
 let cell_type = named_struct_type context "cell";;
-struct_set_body cell_type [|address_type; address_type|] false;
 
-let arg_type = Array.make 2 address_type in
-let ft = function_type cell_type arg_type in
-let the_function = declare_function "cons" ft the_module in
-let car = param the_function 0 in
-let cdr = param the_function 1 in
-let bb = append_block context "entry" the_function in
-position_at_end bb builder;
-try
-  let ret_val = const_named_struct cell_type [|car; cdr|] in
-  let _ = build_ret ret_val builder in
-  Llvm_analysis.assert_valid_function the_function;
-with e->
-  delete_function the_function;
-  raise e;;
+struct_set_body cell_type [|address_type; address_type|] false;;
 
-let arg_type = Array.make 1 cell_type in
-let ft = function_type address_type arg_type in
-let the_function = declare_function "car" ft the_module in
-let cell = param the_function 0 in
-let car = build_load (build_struct_gep cell 0 "ptr" builder) "ret" builder in
-let bb = append_block context "entry" the_function in
-position_at_end bb builder;
-try
-  let ret_val = car in
-  let _ = build_ret ret_val builder in
-  Llvm_analysis.assert_valid_function the_function;
-with e->
-  delete_function the_function;
-  raise e
+let gen_cons a_module context =
+  let arg_type = Array.make 2 address_type in
+  let ft = function_type cell_type arg_type in
+  let the_function = declare_function "cons" ft a_module in
+  let car = param the_function 0 in
+  let cdr = param the_function 1 in
+  let bb = append_block context "entry" the_function in
+  position_at_end bb builder;
+  try
+    let ret_val = const_named_struct cell_type [|car; cdr|] in
+    let _ = build_ret ret_val builder in
+    Llvm_analysis.assert_valid_function the_function;
+  with e->
+    delete_function the_function;
+    raise e;;
+
+let gen_car a_module context =
+  let arg_type = Array.make 1 (pointer_type cell_type) in
+  let ft = function_type address_type arg_type in
+  let the_function = declare_function "car" ft a_module in
+  let cell = param the_function 0 in
+  let bb = append_block context "entry" the_function in
+  position_at_end bb builder;
+  try
+    let gep = build_struct_gep cell 1 "cons" builder in
+    let elm = build_load gep "car" builder in
+    let ret_val = elm in
+    let _ = build_ret ret_val builder in
+    Llvm_analysis.assert_valid_function the_function;
+  with e->
+    delete_function the_function;
+    raise e
+
+let gen_main a_module context =
+  let arg_type = [||] in
+  let ft = function_type (void_type context) arg_type in
+  let main_function = declare_function "main" ft a_module in
+  let bb = append_block context "entry" main_function in
+  bb;;
+
+gen_cons the_module context;;
+gen_car the_module context;;
+let main_bb = gen_main the_module context in
+position_at_end main_bb builder;;
 
 let rec codegen = function
     Ast.Sexp vals ->
